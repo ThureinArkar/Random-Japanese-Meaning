06fad7c96a450ff76bdd819a0cf1cfe8926d3b31
ThureinArkar <thureinarkar457139@gmail.com>
2022-04-29 20:21:59 +0630

    first commit

---
+++ script.js
@@ -0,0 +1,136 @@
+const display=document.querySelector("input"),
+display1 = document.getElementById("text1"),
+display2 = document.getElementById("text2"),
+display3 = document.getElementById("text3"),
+display4 = document.getElementById("text4"),
+ button=document.querySelector("button"),
+ copyBtn=document.querySelector("span.far"),
+ copyActive=document.querySelector("span.fas");
+//let chars ='あいうえお';
+let chars = [
+  {
+  text1:`I`,
+  },
+  {
+    text1:`You`,
+  },
+  {
+    text1:`That Person`,
+  },
+  {
+    text1:`That Person(More Polite)`,
+  },
+  {
+    text1:`Name Suffix(Adult)`,
+  },
+  {
+    text1:`Name Suffix(Underage)`,
+  },
+  {
+    text1:`Race`,
+  },
+  {
+    text1:`Teacher(Not for work)`,
+  },
+  {
+    text1:`Teacher(For work)`,
+  },
+  {
+    text1:`Student(male & female)`,
+  },
+  {
+    text1:`Company Employee(Not allowed company name)`,
+  },
+  {
+    text1:`Company Employee(allowed company name)`,
+  },
+  {
+    text1:`Bank Employee`,
+  },
+  {
+    text1:`Doctor`,
+  },
+  {
+    text1:`Researcher`,
+  },
+  {
+    text1:`University`,
+  },
+  {
+    text1:`Hospital & Clinic`,
+  },
+  {
+    text1:`Who/Which Person?`,
+  },
+  {
+    text1:`Who/Which Person?(More Polite)`,
+  },
+  {
+    text1:`Year/It means age`,
+  },
+  {
+    text1:`How many years?/How old?`,
+  },
+  {
+    text1:`How many years?/How old?(More Polite)`,
+  },
+  {
+    text1:`Yes`,
+  },
+  {
+    text1:`No`,
+  },
+];
+
+ button.onclick=()=>{
+
+let randomPhrase=Math.floor(Math.random() * chars.length);
+// display.value = chars[randomPhrase].text;
+// display1.value = chars[randomPhrase].author;
+display2.value = chars[randomPhrase].text1;
+// display3.value = chars[randomPhrase].text2;
+// let randomPhrase1 =Math.floor(Math.random() * chars1.length);
+// display1.value = chars1[randomPhrase].author;
+   //
+   // let i,
+   // randomPassword="";
+   // copyBtn.style.display = "block";
+   // copyActive.style.display = "none";
+   // for ( i = 0; i < 1; i++) {
+   //   randomPassword = randomPassword + chars.charAt(
+   //     Math.floor(Math.random() * chars.length)
+   //   );
+   // }
+
+ }
+
+
+
+
+
+
+
+
+
+
+function copy() {
+  copyBtn.style.display = "none";
+  copyActive.style.display = "block";
+  display.select();
+  document.execCommand("copy");
+}
+
+$("span.far").click(function () {
+
+  $(".alert").fadeIn(1000);
+  $(".alert").fadeOut(1000);
+
+})
+
+snowFall.snow(document.querySelector('.snow'), {
+	minSize: 6,
+	maxSize: 10,
+	round: true,
+	flakeCount: 30,
+	flakeColor: 'aqua'
+});
